911869e7f2ba807d120ffc6bd9fc53ed2440f848
Joao Antonio <joaopsilva.dev@gmail.com>
2022-03-22 16:15:33 -0300

    requisito 4 completo

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './Helper';
+
+test('Apresentar No favorite pokemon found sem pokemons', () => {
+  renderWithRouter(<FavoritePokemons />);
+  const noPoke = screen.getByText('No favorite pokemon found');
+  expect(noPoke).toBeInTheDocument();
+});
+
+test('Se existir pokemons mostrar pokemon', () => {
+  renderWithRouter(<FavoritePokemons />);
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './Helper';
+
+test('Se a página about contém um heading com o texto About Pokedex', () => {
+  renderWithRouter(<NotFound />);
+  const notFound = screen.getByRole('heading',
+    { level: 2, name: /Page requested not found/i });
+  expect(notFound).toBeInTheDocument();
+});
+
+test('Se a página contém a imagem correta', () => {
+  renderWithRouter(<NotFound />);
+  const image = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  const imageNotFound = screen.getAllByRole('img');
+  expect(imageNotFound[1].src).toBe(image);
+});
