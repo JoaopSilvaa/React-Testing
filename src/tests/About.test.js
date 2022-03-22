befc598f0d9fa84ebc60998b2ad66f88c381b92d
Joao Antonio <joaopsilva.dev@gmail.com>
2022-03-22 16:01:32 -0300

    requisito 2 completo

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import About from '../components/About';
+import renderWithRouter from './Helper';
+
+test('Se a página about contém um heading com o texto About Pokedex', () => {
+  renderWithRouter(<About />);
+  const aboutPoke = screen.getByRole('heading',
+    { level: 2, name: 'About Pokédex' });
+  expect(aboutPoke).toBeInTheDocument();
+});
+
+test('Se a página contém as informações sobre a pokedex', () => {
+  renderWithRouter(<About />);
+  const paragraph1 = screen.getByText(/This application simulates/i);
+  const paragraph2 = screen.getByText(/One can filter Pokémons/i);
+  expect(paragraph1).toBeInTheDocument();
+  expect(paragraph2).toBeInTheDocument();
+});
+
+test('Se a página contém a imagem correta', () => {
+  renderWithRouter(<About />);
+  const image = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  const imagePoke = screen.getByRole('img');
+  expect(imagePoke.src).toBe(image);
+});
